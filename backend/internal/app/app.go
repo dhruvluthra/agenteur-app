4a70e9959700d6f30fb3391a5bd7928577da0efb
Dhruv Luthra <dhruvluthra1@gmail.com>
2026-02-15 15:54:10 -0500

    feat: initial database connection from backend

--- backend/internal/app/app.go
+++ backend/internal/app/app.go
@@ -1,21 +1,32 @@
 package app
 
 import (
+	"context"
 	"log"
 	"net/http"
 
 	"agenteur.ai/api/internal/config"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5"
 )
 
 type App struct {
 	Config *config.Config
 	Server *http.Server
+	DB     *pgx.Conn
 }
 
 func NewApp() *App {
 	config := config.Load()
+	db, err := pgx.Connect(context.Background(), config.DatabaseURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := db.Ping(context.Background()); err != nil {
+		log.Fatal("db ping failed:", err)
+	}
+	log.Println("db connected successfully")
 	server := &http.Server{
 		Addr:    config.Port,
 		Handler: NewRouter(),
@@ -23,11 +34,12 @@ func NewApp() *App {
 	return &App{
 		Config: config,
 		Server: server,
+		DB:     db,
 	}
 }
 
 func (a *App) Start() error {
-	log.Println("Starting server on port", a.Config.Port)
+	log.Println("starting server on port", a.Config.Port)
 	return a.Server.ListenAndServe()
 }
 
